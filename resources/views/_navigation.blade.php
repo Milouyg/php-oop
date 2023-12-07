05c403eb955056388bd1b9f5b63883cb8d427db8
Milouyg <33333@ma-web.nl>
2023-12-07 10:53:23 +0100

    Added active  nav in projects

--- resources/views/_navigation.blade.php
+++ resources/views/_navigation.blade.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<nav class="flex gap-4 items-center">
-            <li class="hover:cursor-pointer @if(Route::currentRouteName() == 'homepage') text-red-500 @else text-red-500 @endif text-black">
-                <a href="{{ route('homepage') }}">Home</a> 
-            </li>
-            <li class="hover:cursor-pointer @if(Route::currentRouteName() == 'projects.project') text-red-500 @else text-red-500 @endif text-black">
-                <a href="{{ route('projects.project') }}">Projecten</a> 
-            </li>
-            <li class="hover:cursor-pointer">
-                <a href="">Contact</a> 
-            </li>
-        </nav>
-</body>
-</html>
+<header class="flex flex-row justify-between px-8 h-1/5">
+    <h1 class="flex text-3xl font-bold underline items-center"><a href="{{ route('homepage') }}">Logo</a></h1>
+    <nav class="flex gap-4 items-center">
+        <li class="hover:cursor-pointer list-none">
+            <a href="{{ route('homepage') }}" class="no-underline {{ (Route::currentRouteName() == 'homepage') ? 'text-red-500' : 'text-black' }}">Home</a>
+        </li>
+        <li class="hover:cursor-pointer list-none">
+            <a href="{{ route('projects.project') }}" class="no-underline {{ (Route::currentRouteName() == 'projects.project') ? 'text-red-500' : 'text-black' }}">Projecten</a> 
+        </li>
+        <li class="hover:cursor-pointer list-none">
+            <a href="">Contact</a> 
+        </li>
+    </nav>
+</header>
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,36 +9,15 @@
 </head>
 
 <body class="flex gap-4 flex-col h-screen">
-    <header class="flex flex-row justify-between px-8 h-1/5">
-        <h1 class="flex text-3xl font-bold underline items-center"><a href="{{ route('homepage') }}">Logo</a></h1>
-        @include('_navigation')
-    </header>
+    @include('_navigation')
     <section class="px-8 flex flex-col items-center h-3/5">
-        <h1 class="font-bold">Over ons</h1>
+        <h1 class="font-bold">Welkom!</h1>
         <p class="">
-            Laravel is een open-source PHP-webframework dat is ontworpen om het ontwikkelingsproces van webapplicaties te versnellen en te vereenvoudigen. Het werd voor het eerst gelanceerd door Taylor Otwell in 2011 en heeft sindsdien snel aan populariteit gewonnen vanwege zijn elegante syntaxis, krachtige functies en uitgebreide gemeenschapsondersteuning.
-
-            Waarom Laravel?
-            1. Eenvoudige Syntax
-            Een van de belangrijkste redenen voor de populariteit van Laravel is de eenvoudige en expressieve syntax. Laravel maakt gebruik van de moderne PHP-functies en biedt ontwikkelaars een elegante manier om code te schrijven, waardoor het gemakkelijker wordt om complexe taken uit te voeren.
-
-            2. MVC Architectuur
-            Laravel volgt het Model-View-Controller (MVC)-ontwerppatroon, wat zorgt voor een duidelijke scheiding van logica en presentatie. Dit maakt de codebase gestructureerd, gemakkelijk te begrijpen en bevordert herbruikbaarheid van code.
-
-            3. Artisan Command Line Tool
-            Laravel wordt geleverd met Artisan, een krachtige command line tool waarmee ontwikkelaars taken kunnen automatiseren, zoals het genereren van code, het uitvoeren van database migraties en het beheren van applicatieconfiguraties.
-
-            4. Eloquent ORM
-            Het Eloquent ORM (Object-Relational Mapping) van Laravel biedt een intuïtieve manier om met databases te communiceren. Het maakt het mogelijk om database-interacties uit te voeren met behulp van objectgeoriënteerde syntax, wat de ontwikkeltijd verkort en de leesbaarheid verbetert.
-
-            5. Blade Templating Engine
-            Laravel bevat de Blade templating engine, die krachtige maar eenvoudige templatingmogelijkheden biedt. Met Blade kunnen ontwikkelaars gestructureerde en herbruikbare views maken.
-
-            6. Laravel Ecosystem
-            Laravel heeft een bloeiend ecosysteem met talloze pakketten en extensies die ontwikkelaars kunnen gebruiken om functionaliteit aan hun applicaties toe te voegen. Het Laravel Package System vereenvoudigt het beheer van externe pakketten.
-
-            Conclusie
-            Laravel heeft zich bewezen als een krachtig PHP-framework voor het bouwen van moderne, schaalbare webapplicaties. Het combineert eenvoudige syntaxis, MVC-architectuur, handige tools zoals Artisan en een actieve gemeenschap om een ontwikkelaarsvriendelijke omgeving te creëren. Of je nu een beginnende of ervaren ontwikkelaar bent, Laravel biedt een solide basis voor het snel en efficiënt ontwikkelen van webapplicaties.
+            Hallo en hartelijk welkom op de officiële website van Amsterdam, de bruisende hoofdstad van Nederland! Amsterdam Ontdekt is jouw digitale gids voor alles wat deze prachtige stad te bieden heeft. Of je nu een local bent die op zoek is naar nieuwe hotspots, een toerist die de unieke charme van Amsterdam wil verkennen, of iemand die gewoonweg nieuwsgierig is naar deze levendige metropool, we hebben voor elk wat wils.
+            Ontdek de rijke geschiedenis van de stad, duik in de diverse cultuur, en laat je inspireren door de verborgen pareltjes die Amsterdam te bieden heeft. Van de sfeervolle grachten en historische architectuur tot de levendige markten en trendy wijken, Amsterdam heeft voor ieder wat wils.
+            Op onze website vind je nuttige informatie, tips van locals, evenementenkalenders en nog veel meer. Of je nu op zoek bent naar de beste restaurants, de leukste winkelstraten, of gewoon wat praktische tips voor je bezoek, Amsterdam Ontdekt staat voor je klaar.
+            Laat je inspireren, plan je bezoek, en ervaar Amsterdam op zijn best. Ontdek de verborgen schatten, geniet van de lokale lekkernijen en wordt verliefd op de unieke sfeer van deze betoverende stad.
+            Veel plezier met het ontdekken van Amsterdam!
         </p>
     </section>
     <footer class="px-8 h-1/5 flex justify-center items-center">
